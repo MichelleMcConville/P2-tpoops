f7c2090f0f626dc8b437c7d7d6014f2095c9c082
Michelle McConville <mchelmconvl@gmail.com>
2020-12-22 18:59:58 -0600

    fixed lint errors

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,34 +1,34 @@
 //
-const db = require("../models");
+const db = require('../models');
 const itemController = {
-  createItem: function(req, res) {
-    db.Item.create(req.body).then(function(results) {
+  createItem: (req, res) => {
+    db.Item.create(req.body).then((results) => {
       res.json(results);
     });
   },
-  getItems: function(req, res) {
-    db.Item.findAll().then(function(results) {
+  getItems: (req, res) => {
+    db.Item.findAll().then((results) => {
       res.json(results);
     });
   },
-  updateItem: function(req, res) {
+  updateItem: (req, res) => {
     db.Item.update({
       where: {
-        id: req.params.id,
-      },
-    }).then(function(results) {
+        id: req.params.id
+      }
+    }).then((results) => {
       res.json(results);
     });
   },
-  deleteItem: function(req, res) {
+  deleteItem: (req, res) => {
     db.Item.destroy({
       where: {
-        id: req.params.id,
-      },
-    }).then(function(results) {
+        id: req.params.id
+      }
+    }).then((results) => {
       res.json(results);
     });
-  },
+  }
 };
 
 module.exports = itemController;
