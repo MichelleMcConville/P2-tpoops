a5aa18a0e3bcb3e486c073ea3ce1f04c981a87e5
KobeHill68 <kobehill68@gmail.com>
2020-12-25 13:02:07 -0600

    fixing lint errors

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,33 +1,33 @@
 const db = require("../models");
 const userController = {
-    createUser: (req, res) => {
-        db.User.create(req.body).then((results) => {
-            res.json(results)
-        })
-    },
-    getUser: (req, res) => {
-        db.USer.findAll(req.body).then((results) => {
-            res.json(results)
-        })
-    },
-    updateUser: (req, res) => {
-        db.User.update(req.body, {
-            where: {
-                id: req.params.id
-            }
-        }).then((results) => {
-            res.json(results)
-        })
-    },
-
-    deleteUser: (req, res) => {
-        db.User.destroy(req.body, {
-            where: {
-                id: req.params.id
-            }
-        }).then((results) => {
-            res.json(results)
-        })
-    }
+  createUser: (req, res) => {
+    db.User.create(req.body).then((results) => {
+        res.json(results);
+    });
+  },
+  getUser: (req, res) => {
+    db.User.findAll().then((results) => {
+        res.json(results);
+    });
+  },
+  updateUser: (req, res) => {
+    db.User.update(req.body, {
+        where: {
+        id: req.params.id
+        }
+    }).then((results) => {
+        res.json(results);
+    });
+  },
+  deleteUser: (req, res) => {
+    db.User.destroy(req.body, {
+        where: {
+        id: req.params.id
+        }
+    }).then((results) => {
+        res.json(results);
+    });
+  }
 };
+
 module.exports = userController;
