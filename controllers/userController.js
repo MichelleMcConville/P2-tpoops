2ba84b2d0dcfc94b27bd18a16e23c6c62deb1f28
KobeHill68 <kobehill68@gmail.com>
2020-12-25 13:05:14 -0600

    fixing lint more lint errors

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,31 +1,31 @@
-const db = require("../models");
+const db = require('../models');
 const userController = {
   createUser: (req, res) => {
     db.User.create(req.body).then((results) => {
-        res.json(results);
+      res.json(results);
     });
   },
   getUser: (req, res) => {
     db.User.findAll().then((results) => {
-        res.json(results);
+      res.json(results);
     });
   },
   updateUser: (req, res) => {
     db.User.update(req.body, {
-        where: {
+      where: {
         id: req.params.id
-        }
+      }
     }).then((results) => {
-        res.json(results);
+      res.json(results);
     });
   },
   deleteUser: (req, res) => {
     db.User.destroy(req.body, {
-        where: {
+      where: {
         id: req.params.id
-        }
+      }
     }).then((results) => {
-        res.json(results);
+      res.json(results);
     });
   }
 };
