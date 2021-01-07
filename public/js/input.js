cbb344e326721679083e4743c474f91e0ab04134
KHallam <kjhallam321@gmail.com>
2021-01-06 20:20:57 -0600

    fixed input page to submit to search page

--- public/js/input.js
+++ public/js/input.js
@@ -1,11 +1,11 @@
-$(document).ready(() => {
+$(document).ready(function () {
   const inputForm = $('form.input');
   const storeInput = $('input#store-input');
   const cityInput = $('input#city-input');
   const stateInput = $('input#state-input');
   const zipCodeInput = $('input#zip-code-input');
 
-  inputForm.on('submit', event => {
+  inputForm.on('submit', function (event) {
     event.preventDefault();
     const itemData = {
       store: storeInput.val().trim(),
@@ -24,20 +24,20 @@ $(document).ready(() => {
     zipCodeInput.val('');
   });
 
-  const inputData = (store, city, state, zipcode) => {
-    $.post('api/input', {
+  const inputData = function (store, city, state, zipcode) {
+    $.post('/api/input', {
       store: store,
       city: city,
       state: state,
       zipcode: zipcode
     })
-      .then(() => {
-        window.location.replace('/input');
+      .then(function () {
+        location.href="/search"
       })
       .catch(handleInputErr);
   };
 
-  const handleInputErr = err => {
+  const handleInputErr = function (err) {
     $('#alert .msg').text(err.responseJSON);
     $('#alert').fadeIn(500);
   };
